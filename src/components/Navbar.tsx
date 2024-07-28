01f1640df05a453833685dd8712582a98ea4717f
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2024-07-28 06:39:55 +0200

    Made navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { FaCode, FaXmark } from "react-icons/fa6";
 import { CgMenuRight } from "react-icons/cg";
@@ -8,7 +8,13 @@ import { Button } from "./";
 import "../css/components/Navbar.css";
 
 const Navbar: React.FC = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false);
+
   const location = useLocation();
+
+  const handleToggleMenu = () => {
+    setMenuIsOpen((prevState) => !prevState);
+  };
 
   if (location.pathname === "/note") return;
 
@@ -18,50 +24,65 @@ const Navbar: React.FC = () => {
         <FaCode style={{ fontSize: "3rem" }} />
         <h2>Codester</h2>
       </Link>
-      <Button className="menu">
+      <Button className="menu" onClick={handleToggleMenu}>
         <CgMenuRight style={{ fontSize: "2.25rem" }} />
       </Button>
-      <div className="background">
-        <ul className="navigation">
+      <div
+        className={`background ${menuIsOpen ? "open" : ""}`}
+        onClick={handleToggleMenu}
+      >
+        <ul className="navigation" onClick={(event) => event.stopPropagation()}>
           <li className="close-menu">
-            <Button>
+            <Button onClick={handleToggleMenu}>
               <FaXmark style={{ fontSize: "2.25rem" }} />
             </Button>
           </li>
           <li style={{ marginBottom: "3rem" }}>
-            <Button to="/note" primary>
+            <Button to="/note" primary onClick={handleToggleMenu}>
               Create a note
             </Button>
           </li>
           {false && (
             <>
               <li>
-                <Button to="/login">Log in</Button>
+                <Button to="/login" onClick={handleToggleMenu}>
+                  Log in
+                </Button>
               </li>
               <li>
-                <Button to="/signup">Sign up</Button>
+                <Button to="/signup" onClick={handleToggleMenu}>
+                  Sign up
+                </Button>
               </li>
             </>
           )}
           {true && (
             <>
               <li>
-                <Button to="/my-notes">My notes</Button>
+                <Button to="/my-notes" onClick={handleToggleMenu}>
+                  My notes
+                </Button>
               </li>
               <li>
-                <Button to="/following">Following</Button>
+                <Button to="/following" onClick={handleToggleMenu}>
+                  Following
+                </Button>
               </li>
               <li>
-                <Button to="/settings">Settings</Button>
+                <Button to="/settings" onClick={handleToggleMenu}>
+                  Settings
+                </Button>
               </li>
             </>
           )}
           <li>
-            <Button to="/trending">Trending</Button>
+            <Button to="/trending" onClick={handleToggleMenu}>
+              Trending
+            </Button>
           </li>
           {true && (
             <li>
-              <Button>Log out</Button>
+              <Button onClick={handleToggleMenu}>Log out</Button>
             </li>
           )}
         </ul>
